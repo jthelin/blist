322384d43562a97e9c1f99e0cbe45ae3f8d0af68
Jorgen Thelin <jthelin@microsoft.com>
2019-08-04 09:48:02 -0700

    ParseArguments function.
    - Add `blist_params` data object for parameter values.
    - Add `ParseArguments` function to separate out command line argument handling.

--- blist.h
+++ blist.h
@@ -1,6 +1,15 @@
 #ifndef BLIST_BLIST_H
 #define BLIST_BLIST_H
 
+#include <iostream>
+
 int blist_main(int argc, char **argv);
+
+// Call as (for example)  std::cout << NL;
+// Function: NL - Output a new line onto a stream.
+inline std::ostream &NL(std::ostream &s) { return (s.put('\n')); }
+
+// Function: FF - Output a form feed (new page) onto a stream.
+inline std::ostream &FF(std::ostream &s) { return (s.put('\f')); }
 
 #endif //BLIST_BLIST_H
