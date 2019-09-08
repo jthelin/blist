7c52d9a83f57b4bcaa6a24dcb00de08566ab6478
Jorgen Thelin <jthelin@microsoft.com>
2019-09-07 19:43:58 -0700

    Use getopt.h compatible with Windows rather than only Linux.

---
+++ libsrc/getopt.h
@@ -0,0 +1,18 @@
+#ifndef _GETOPT_H
+#define _GETOPT_H
+
+extern int
+  opterr,    /* if error message should be printed */
+  optind,    /* index into parent argv vector */
+  optopt,    /* character checked for validity */
+  optreset;  /* reset getopt */
+
+extern char *optarg;  /* argument associated with option */
+
+/*
+* getopt --
+*      Parse argc/argv argument vector.
+*/
+extern int getopt(int nargc, char * const nargv[], const char *ostr);
+
+#endif // _GETOPT_H
