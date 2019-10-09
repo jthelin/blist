1d6b5a310c5b713dc4f3d4dc2cea54f3bf83f81d
Jorgen Thelin <jthelin@microsoft.com>
2019-10-08 21:38:30 -0700

    Use url download rather than git clone for googletest package.

--- cmake/googletest.cmake
+++ cmake/googletest.cmake
@@ -11,8 +11,9 @@ set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 INCLUDE(FetchContent)
 
 FetchContent_Declare(googletest
-  GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG        release-1.10.0
+  URL         https://github.com/google/googletest/archive/release-1.10.0.tar.gz
+  URL_HASH    SHA256=9dc9157a9a1551ec7a7e43daea9a694a0bb5fb8bec81235d8a1e6ef64c716dcb
+  SOURCE_DIR  googletest-src
 )
 
 FetchContent_GetProperties(googletest)
