aec2cd990553eaffa7bf8a9c3b4b7998a8faf7f1
Jorgen Thelin <jthelin@microsoft.com>
2020-09-11 10:38:19 -0700

    Shellscript updates (#51)
    - Use `cmake-build` directory.
    - Always resolve `cmake-build` directory relative to repo root.
    - Exit `build.sh` script immediately if a command exits with a non-zero status.

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-BLD_DIR=build
+CMD_DIR=$(dirname "$0")
 
-mkdir -p "${BLD_DIR}" && pushd "${BLD_DIR}" || return
+BLD_DIR=${CMD_DIR}/cmake-build
+
+mkdir -p "${BLD_DIR}" && pushd "${BLD_DIR}" || exit 1
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+set -o pipefail
 
 echo "---- cmake generate ----"
-set -x
 cmake .. &&
   echo "---- cmake build ----" &&
+  set -x &&
   cmake --build . "$@"
