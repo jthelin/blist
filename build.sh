c9ca296c6e61f5a43844422765cec393a9e49688
jthelin <jthelin@microsoft.com>
2020-10-04 19:32:50 -0700

    cmake install

--- build.sh
+++ build.sh
@@ -2,16 +2,23 @@
 
 CMD_DIR=$(dirname "$0")
 
-BLD_DIR=${CMD_DIR}/cmake-build
+BUILD_DIR=${CMD_DIR}/cmake-build
+INSTALL_DIR=_install
 
-mkdir -p "${BLD_DIR}" && pushd "${BLD_DIR}" || exit 1
+mkdir -p "${BUILD_DIR}" && pushd "${BUILD_DIR}" || exit 1
+mkdir -p "${INSTALL_DIR}" || exit 1
 
 # Exit immediately if a command exits with a non-zero status.
 set -e
 set -o pipefail
 
 echo "---- cmake generate ----"
-cmake .. &&
-  echo "---- cmake build ----" &&
-  set -x &&
-  cmake --build . "$@"
+cmake ..
+
+echo "---- cmake build ----" &&
+set -x
+cmake --build . "$@"
+
+echo "---- cmake install ----" &&
+cmake --install . --prefix "${INSTALL_DIR}"
+ls -lhR "${PWD}/${INSTALL_DIR}"
