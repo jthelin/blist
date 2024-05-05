7c63ab9747335ef7be0953845af77b54cffe3c01
jthelin <jthelin@microsoft.com>
2024-05-05 01:13:27 -0700

    clang-format

--- src/blist.h
+++ src/blist.h
@@ -22,13 +22,11 @@ int blist_main(int argc, char** argv);
 
 // Call as (for example)  std::cout << NL;
 // Function: NL - Output a new line onto a stream.
-inline std::ostream& NL(std::ostream& s)
-{
+inline std::ostream& NL(std::ostream& s) {
   return s.put('\n');
 }
 
 // Function: FF - Output a form feed (new page) onto a stream.
-inline std::ostream& FF(std::ostream& s)
-{
+inline std::ostream& FF(std::ostream& s) {
   return s.put('\f');
 }
